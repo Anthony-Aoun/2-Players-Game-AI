7176c69f3ddf42d843aaad3f2993975c181c28d3
AnthonyAoun7 <anthony.aoun2000@gmail.com>
2020-04-24 17:07:15 +0200

    checkers eval

--- IN104_PROJECT_AOUN_BENNATAN/evaluation_functions/checkers.py
+++ IN104_PROJECT_AOUN_BENNATAN/evaluation_functions/checkers.py
@@ -1,4 +1,18 @@
-from aiarena.checkers import cell
+import aiarena 
 
+#returns difference of pieces
 def evaluate(gameState):
-    raise NotImplementedError()
+    white = 0
+    black = 0
+    for c in gameState.cells:
+        if (c.isWhite == True) and (c.type != aiarena.checkers.cell.NONE):
+            white += 1
+
+        elif c.isWhite == False and (c.type != aiarena.checkers.cell.NONE):
+            black += 1
+
+    return (white-black)
+
+
+
+    
