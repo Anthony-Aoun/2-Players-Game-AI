d569a939010660d83410c9a26de3fafbfbc979f2
AnthonyAoun7 <anthony.aoun2000@gmail.com>
2020-05-11 22:07:01 +0200

    First try transposition table

--- IN104_PROJECT_AOUN_BENNATAN/minimax/limited_depth_tt.py
+++ IN104_PROJECT_AOUN_BENNATAN/minimax/limited_depth_tt.py
@@ -12,29 +12,29 @@ def transposition(node, maximize, get_children, evaluate, max_depth,table):
     if (maximize == True):
         best = -(np.Inf)
         for child in get_children(node):
+            #If the child has been visited we just take its value without exploring its children
+            if child in table:
+                value = table[child]
+                best = max(best,value)             
             #If the child hasn't been visited, we visit it and add it to table
-            if not (child in table):
+            else:
                 value = minimax(child, False, get_children, evaluate, max_depth-1)
                 table.update({child : value})
                 best = max(best, value)
-            #If the child has been visited we just take its value without exploring its children
-            else:
-                value = table[child]
-                best = max(best,value)
         return best
 
     elif (maximize == False):
         best = np.Inf
         for child in get_children(node):
-            #If the child hasn't been visited, we visit it and add it to table
-            if not (child in table):
-                value = minimax(child, True, get_children, evaluate, max_depth-1)
-                table.update({child : value})
-                best = min(best, value)
             #If the child has been visited we just take its value without exploring its children
-            else:
+            if child in table:
                 value = table[child]
                 best = max(best,value)
+            #If the child hasn't been visited, we visit it and add it to table
+            else:
+                value = minimax(child, True, get_children, evaluate, max_depth-1)
+                table.update({child : value})
+                best = min(best, value)      
         return best
 
 def minimax(node, maximize, get_children, evaluate, max_depth):
