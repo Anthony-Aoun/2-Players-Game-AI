b97a2ae258f763f6bffa657814e4ba5e82f05f4e
AnthonyAoun7 <anthony.aoun2000@gmail.com>
2020-05-13 22:01:27 +0300

    limited_time_negascout_tt.py

---
+++ IN104_PROJECT_AOUN_BENNATAN/minimax/limited_time_negascout_tt.py
@@ -0,0 +1,49 @@
+#white is maximizing
+#black is minimizing
+
+import numpy as np
+import time
+
+def nega_transp(node, maximize, get_children, evaluate, maxTime, a, b, table):
+    if (maxTime <= 0 or (get_children(node) == [])):
+        if maximize:
+            return evaluate(node)
+        else:
+            return -evaluate(node)
+
+    elapsed=0
+    childNumber = len(get_children(node))
+    for i,child in enumerate(get_children(node)):
+        tic = time.time()
+
+        #If the child has been visited we just take its value without exploring its children
+        if child.toString() in table:
+            score = table[child.toString()]
+
+        #If the child hasn't been visited, we visit it and add it to table
+        else:
+            if i!=0:
+                score = -nega_transp(child,not(maximize),get_children,evaluate,(maxTime-elapsed)/(childNumber-i),-a-1,-a,table)
+
+                if a<score and score<b:
+                    score = -nega_transp(child,not(maximize),get_children,evaluate,(maxTime-elapsed)/(childNumber-i),-b,-score,table)
+
+            else:
+                score = -nega_transp(child,not(maximize),get_children,evaluate,(maxTime-elapsed)/(childNumber-i),-b,-a,table)
+
+            table.update({child.toString() : score})
+
+        a = max(a,score)
+
+        if a >= b:
+            break
+        
+        elapsed += time.time() - tic
+
+    return a
+
+def minimax(node, maximize, get_children, evaluate, maxTime):
+    return nega_transp(node, maximize, get_children, evaluate, maxTime, -(np.Inf), np.Inf,{})
+
+    
+
