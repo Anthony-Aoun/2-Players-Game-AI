e0b39366c694d3c5719b7032a8451702c17bb37f
AnthonyAoun7 <anthony.aoun2000@gmail.com>
2020-05-13 17:00:52 +0200

    transposition table & alphabeta+tranposition table

---
+++ IN104_PROJECT_AOUN_BENNATAN/minimax/limited_depth_alphabeta_tt.py
@@ -0,0 +1,43 @@
+#white is maximizing
+#black is minimizing
+
+#table is a dictionnary {state : score}
+
+import numpy as np
+
+def alphabeta_transp(node, maximize, get_children, evaluate, max_depth,a,b,table):
+    if (max_depth == 0 or (get_children(node) == [])):
+        return evaluate(node)
+    
+    if (maximize == True):
+        for child in get_children(node):
+            #If the child has been visited we just take its value without exploring its children
+            if child in table:
+                value = table[child]
+                a = max(a,value)             
+            #If the child hasn't been visited, we visit it and add it to table
+            else:
+                value = alphabeta_transp(child, False, get_children, evaluate, max_depth-1,a,b,table)
+                a = max(a,value)
+                table.update({child : value})
+                if a >= b:
+                    break
+        return a
+
+    elif (maximize == False):
+        for child in get_children(node):
+            #If the child has been visited we just take its value without exploring its children
+            if child in table:
+                value = table[child]
+                b = min(b,value)
+            #If the child hasn't been visited, we visit it and add it to table
+            else:
+                value = alphabeta_transp(child, True, get_children, evaluate, max_depth-1,a,b,table)
+                b = min(b,value)
+                table.update({child : value})
+                if a >= b:
+                    break    
+        return b
+
+def minimax(node, maximize, get_children, evaluate, max_depth):
+    return alphabeta_transp(node, maximize, get_children, evaluate, max_depth,-(np.Inf), np.Inf, {})
--- IN104_PROJECT_AOUN_BENNATAN/minimax/limited_depth_tt.py
+++ IN104_PROJECT_AOUN_BENNATAN/minimax/limited_depth_tt.py
@@ -18,7 +18,7 @@ def transposition(node, maximize, get_children, evaluate, max_depth,table):
                 best = max(best,value)             
             #If the child hasn't been visited, we visit it and add it to table
             else:
-                value = minimax(child, False, get_children, evaluate, max_depth-1)
+                value = transposition(child, False, get_children, evaluate, max_depth-1,table)
                 table.update({child : value})
                 best = max(best, value)
         return best
@@ -29,10 +29,10 @@ def transposition(node, maximize, get_children, evaluate, max_depth,table):
             #If the child has been visited we just take its value without exploring its children
             if child in table:
                 value = table[child]
-                best = max(best,value)
+                best = min(best,value)
             #If the child hasn't been visited, we visit it and add it to table
             else:
-                value = minimax(child, True, get_children, evaluate, max_depth-1)
+                value = transposition(child, True, get_children, evaluate, max_depth-1,table)
                 table.update({child : value})
                 best = min(best, value)      
         return best
